84f772f6afe28135834ecfa61cfe4f2b40198d57
Yassinbenhaddou <43605983+Yassinbenhaddou@users.noreply.github.com>
2022-01-28 02:00:00 +0100

    pushh

--- Test Img Track/Assets/Loop.cs
+++ Test Img Track/Assets/Loop.cs
@@ -7,14 +7,28 @@ public class Loop : MonoBehaviour
     // Start is called before the first frame update
 
     public float loopSpeed;
+
+    public bool loop;
     void Start()
     {
-        
+        loop = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+      if (Input.touchCount > 0)
+        {
+          //set loop to false
+          loop = false;
+          loopSpeed = 0;
+          //start the coroutine
+          StartCoroutine(CouroutineSetTrue());
+        }
+
+      if(loop)
+      {
 
         loopSpeed += 0.7f; 
 
@@ -25,5 +39,18 @@ public class Loop : MonoBehaviour
           loopSpeed = 0f;
         }
         
+
+      }
+
+        
+    }
+
+
+    IEnumerator CouroutineSetTrue()
+    {
+        //Wait 3 seconds before resuming the loop
+        yield return new WaitForSeconds(3);
+
+       loop = true;
     }
 }
